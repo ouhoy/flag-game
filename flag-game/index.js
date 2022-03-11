d7a9b4bbdb9bf26adc8f27b8eaf054ece8bcb4e4
ouhoy <kimkinon@gmail.com>
2022-03-11 23:16:37 +0100

    Add some lines to the code

--- flag-game/index.js
+++ flag-game/index.js
@@ -1,4 +1,19 @@
 let score = 0;
+// class Country {
+//     constructor(name, colors,flag,capital, image){
+//         this.name = name,
+//         this.colors = colors,
+//         this.flag = flag,
+//         this.capital = capital,
+//         this.image = image
+//     };
+//     get(){
+//         this.name;
+//     }
+//     push(){
+
+//     }
+// }
 const country = {
     Morocco: {
         name: "Morocco",
@@ -9,23 +24,33 @@ const country = {
         name: "Algeris",
         flag: ["red","green","white"],
         code: 3
+    },
+    Turkey:{
+        name: "Ankara",
+        flag:["red","white"],
+        code: 2
     }
 }
-const countries = ["Morocco", "Algeria", "Serbia"];
+const countries = [country.Morocco, country.Algeria, country.Turkey];
 function startGame() {
 let userGuess1 = ["red", "green"]
 
-   setInterval(function(){
-       console.log("time done!")
-   }, 420)
+//    setInterval(function(){
+//        console.log("time done!")
+//    }, 420)
  while (countries.length > 0){
      console.log("remaining countries: ", countries.length)
     for(let i = 0; i < countries.length; i++){
-        for(let i = 0; i< country.Morocco.flag.length; i++){
-            if (userGuess1[i] !=  country.Morocco.flag[i]){
+        console.log(i)
+        console.log(countries[i].flag[i])
+        
+        for(let i = 0; i< countries[i].flag.length; i++){
+            if (userGuess1[i] !=  countries[i].flag[i]){
+                
                 console.log("wrong");
                 countries.pop();
             } else {
+                console.log("else here: " , countries)
                 countries.pop();
                 score++;
             }
@@ -36,3 +61,5 @@ let userGuess1 = ["red", "green"]
  } 
 
 }
+
+startGame()
